410dcad2c29fb76b5227a562fd5e53a3c8628330
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-03 02:57:36 +0600

    Add files via upload

---
+++ Operators(Arithmatic & Assingment)/Operator.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace OP
+{
+    class Operator
+    {
+        static void Main(string[]args)
+        {
+            //[Arithmatic Operator: +, _, *, /, %, ++, --]
+            int x = 1+2;
+            Console.WriteLine(x);
+            Console.WriteLine(34+1);
+            Console.WriteLine("++ -> "+x++);
+            Console.WriteLine(1%3);
+
+            //[Assignment op: +=, -=, *=, /=, %=, &=, |=, ^=, >>=, <<=,]
+            int y = x+=4;
+            Console.WriteLine("+= -> "+y);
+            int z = x-=4;
+            Console.WriteLine("-= ->"+z);
+
+        }
+    }
+}
