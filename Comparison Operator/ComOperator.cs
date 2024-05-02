9178b2e272b2844042f1db14df11d6dbeafcfaf2
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-03 03:00:30 +0600

    Add files via upload

---
+++ Comparison Operator/ComOperator.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace Composition
+{
+    class ComOperator
+    {
+        static void Main(string[]args)
+        {
+            int x = 7;
+            int y = 8;
+            Console.WriteLine("x = "+x);
+            Console.WriteLine("y = "+y);
+            Console.WriteLine("x>y? "+(x>y));
+            Console.WriteLine("x=y? "+(x==y));
+            Console.WriteLine("x<y? "+(x<y));
+        }
+    }
+}
