3289c301b1ad7590326d6d14818ba8c2b8a2c9e2
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-03 02:59:42 +0600

    Add files via upload

---
+++ Logical operator/LogicalOp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace LOperator
+{
+    class LogicalOp
+    {
+        static void Main(string[]args)
+        {
+            int x = 3;
+            int y = 4;
+            Console.WriteLine((x>y)&&(x<y));
+            Console.WriteLine((x>y)||(x<y));
+            Console.WriteLine(!((x>y)&&(x<y)));
+
+        }
+    }
+}
