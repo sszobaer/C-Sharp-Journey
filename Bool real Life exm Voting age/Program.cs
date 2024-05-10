2934be78f71fb83c39fc6a9e25fcba842f01e146
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-10 20:38:30 +0600

    Add files via upload

---
+++ Bool real Life exm Voting age/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace booleans
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int votingAge = 18;
+            Console.WriteLine(age>=votingAge); 
+        }
+    }
+}
