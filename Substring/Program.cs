3121793c0d9cc3712f73ade1f029db52cebe40ab
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-10 20:36:39 +0600

    Add files via upload

---
+++ Substring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace strings
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            /*Substring is a useful method is Substring(), which extracts the characters from a string, 
+            starting from the specified character position/index, and returns a new string. 
+            This method is often used together with IndexOf() to get the specific character position:*/
+            string fullName = "Sara Islam";
+            int CharacterPosition = fullName.IndexOf("I");
+            string lastName = fullName.Substring(CharacterPosition);
+            Console.WriteLine("Last name is: "+lastName);
+
+            //Special Characters
+            Console.WriteLine("I am Pursuing \"CSE\" from \"AIUB\".\n It\'s a dream of my life to get job at google.");
+
+
+        }
+    }
+}
