009208a37e84e8c2a5313d65bf2d9e5c76ebf289
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-12 01:59:07 +0600

    Add files via upload

---
+++ C# Booleans/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Booleans
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            bool IsEmpty = true;
+            bool IsFull = false;
+            Console.WriteLine(IsEmpty);
+            Console.WriteLine(IsFull);
+
+            //Boolean expression
+            int a = 10;
+            int b = 20;
+            Console.WriteLine(a>b);
+        }
+    }
+}
