d8a77958a749b028335964fb89fbd1e97b517a10
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-11 23:11:42 +0600

    Add files via upload

---
+++ ShortHand if else/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ShortHand
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            string output = (age>=18)?"You are eligible for voting. ": "You are Not eligible for voting";
+            Console.WriteLine(output);
+        }
+    }
+}
