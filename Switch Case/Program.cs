63362c900bafb52ccef3404d31b82697f6b0ff63
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-12 02:01:18 +0600

    Add files via upload

---
+++ Switch Case/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+namespace SwitchCase
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            int day = 2;
+            switch(day)
+            {
+                case 1:
+                Console.WriteLine("Sunday");
+                break;
+
+                case 2:
+                Console.WriteLine("Monday");
+                break;
+
+                case 3:
+                Console.WriteLine("Tuesday");
+                break;
+
+                case 4:
+                Console.WriteLine("Wednesday");
+                break;
+
+                case 5:
+                Console.WriteLine("Thursday");
+                break;
+
+                case 6:
+                Console.WriteLine("Friday");
+                break;
+
+                case 7:
+                Console.WriteLine("Saturday");
+                break;
+            }
+        }
+    }
+}
