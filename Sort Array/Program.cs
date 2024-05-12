d169f71a40b928a285fd6e5aac00f7b881d307b1
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-12 23:35:15 +0600

    Add files via upload

---
+++ Sort Array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace SortArray
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            //Sort an array
+            int[] numbers = new int[5] {5, 6, 1, 3, 2};
+            Array.Sort(numbers);
+            foreach (int i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+
+            //sort an string
+            string[] cars={"BMW", "Audi", "Toyata", "Honda"};
+            Array.Sort(cars);
+            foreach(string item in cars)
+            {
+                Console.WriteLine(item);
+            }
+            
+        }
+    }
+}
