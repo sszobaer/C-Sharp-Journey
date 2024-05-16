9c9aafcc5cec6144cf337a513ee9dc06771e08d0
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-16 23:11:49 +0600

    Add files via upload

---
+++ Methods/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Diagnostics;
+namespace Methods
+{
+    class Program
+    {
+        //Parameters & Arguments
+        static void MyMethod()
+        {
+            Console.WriteLine("It's me Zobaer");
+        }
+        static void summation(int a, int b, int c)
+        {
+            int sum = a+b+c;
+            Console.WriteLine($"a+b+c = {sum}");
+        }
+        static void substruction(int a, int b)
+        {
+            int sub = a-b;
+            Console.WriteLine($"a-b = {sub}");
+        }
+
+        static void Multiplication(int a, int b, int c)
+        {
+            int mul = a*b*c;
+            Console.WriteLine($"a * b * c = {mul}");
+        }
+        static void division(int a, int b)
+        {
+            double div = a/b; 
+            Console.WriteLine($"a/b = {div}");
+        }
+
+        //Default parameter
+        static void DreamCountries(string Country = "France")
+        {
+            Console.WriteLine($"My dream country is: {Country}");
+        }
+        static void Main(string[]args)
+        {
+            MyMethod();
+            summation(2,3,5);
+            substruction(6,4);
+            Multiplication(2,3,5);
+            division(10,5);
+            //Default parameter
+            DreamCountries();
+            DreamCountries( "Italy");
+            DreamCountries("Canada");
+            
+        }
+    }
+}
