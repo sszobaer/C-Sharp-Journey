a25df569cb8927b0cc0b725e1d4043ee8c34ccad
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-19 01:49:10 +0600

    Add files via upload

---
+++ Method Overloading/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace MethodOverloading
+{
+    class Program
+    {
+        //When there is same name but different primitive datatype
+        static int sum(int a, int b)=> a+b;
+        static double sum(double a, double b)=> a+b;
+        static void Main(string[]args)
+        {
+            Console.WriteLine($"Int: {sum(2,3)}");
+            Console.WriteLine($"Double: {sum(2.5, 5.6)}");
+        }
+    }
+}
---
+++ Methods Return Values & Named Arguments/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+namespace MethodReturns
+{
+    class Program
+    {
+        //Return values
+        static int func(int a)
+        {
+            return 5+a;
+        }
+
+        static int sum(int x, int y) =>x+y;
+
+        //Named Argument 
+        static void NamedArgument(string name1 , string name2 , string name3  , string name4 )
+        {
+            Console.WriteLine("The intilligent person is: "+name2);
+        }
+        static void Main(string[]args)
+        {
+            Console.WriteLine(func(3));
+            Console.WriteLine(sum(2,4));
+            NamedArgument(name1: "Zobaer",name2: "Sara", name3: "Sohana", name4: "Fahad");
+        }
+    }
+}
