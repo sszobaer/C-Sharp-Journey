6db61ee5d10379508a578441d52167a275f701e3
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-21 02:30:34 +0600

    Add files via upload

---
+++ Methods Problem Solving/PS - 04/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Write a method called Greet that takes two parameters: 
+a string name and an optional string greeting with a default value of "Hello". 
+The method should print the greeting followed by the name.
+ Demonstrate the method with both one and two arguments in the Main method.*/
+using System;
+namespace OptionalParameter
+{
+    class Program
+    {
+        static void Greet(string name, string greeting = "Hello")
+        {
+            Console.WriteLine($"{greeting} {name}");
+        }
+        static void Main(string[]args)
+        {
+            Greet("Zobaer");
+            Greet("Efaz", "Hi");
+        }
+    }
+}
