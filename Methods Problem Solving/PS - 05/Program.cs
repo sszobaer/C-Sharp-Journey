db45db7999426451b0533bad67f19eccbc8fb9c7
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-21 02:34:33 +0600

    Add files via upload

---
+++ Methods Problem Solving/PS - 05/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Write a method called Sum that takes a variable number of integer arguments 
+using the params keyword and returns their sum. 
+Use this method in the Main method to compute and print the sum of various sets of numbers.*/
+using System;
+namespace ParamsKeyword
+{
+    class Program
+    {
+        static int sum(params int[] number)
+        {
+            int total = 0;
+            foreach (int num in number)
+            {
+                total+=num;
+            }
+            return total;
+        }
+        static void Main(string[]args)
+        {
+            Console.WriteLine($"The sum of 3 numbers is: {sum(2,5,6)}");
+            Console.WriteLine($"The sum of 2 numbers is: {sum(4,6)}");
+            Console.WriteLine($"The sum of 6 numbers is: {sum(19,13,54,78,92,100)}");
+        }
+    }
+}
---
+++ Methods Problem Solving/PS - 06/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Create a method called Swap that takes two integers by reference and swaps their values. 
+Demonstrate the use of this method in the Main method by swapping 
+and printing the values of two integers.*/
+using System;
+namespace PassByRef
+{
+    class Program
+    {
+        static void Swap(ref int a,ref int b)
+        {
+            int temp = a;
+            a=b;
+            b=temp;
+        }
+        static void Main(string[]args)
+        {
+            int a = 4, b =6;
+            Swap(ref a, ref b);
+            Console.WriteLine($"The swapping value is: {a},{b}");
+        }
+    }
+}
