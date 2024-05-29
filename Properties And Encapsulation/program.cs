1fe6d655e8cb92703d39694887e2d69ac29beaec
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-05-29 18:28:50 +0600

    Add files via upload

---
+++ Properties And Encapsulation/program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+namespace Encapsulation
+{
+    class student
+    {
+        private string name;
+        private string Id;
+        private string cgpa;
+
+        public string Name
+        {
+            get{return name;}
+            set{name = value;}
+        }
+
+        public string ID
+        {
+            get{return Id;}
+            set{Id = value;}
+        }
+
+        public string CGPA
+        {
+            get{return cgpa;}
+            set{cgpa = value;}
+        }
+
+        public student(string x, string y, string z)
+        {
+            Name = x;
+            ID = y;
+            CGPA =z;
+        }
+    }
+    class program
+    {
+        static void Main(string[]args)
+        {
+            student myObj = new student("Zobaer", "22-49415-3", "3.78");
+           
+            Console.WriteLine($"Name = {myObj.Name}\nID = {myObj.ID} \nCGPA = {myObj.CGPA}");
+        }
+    }
+}
---
+++ Short hand properties/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+namespace ShortHandProperties
+{
+    class student
+    {
+        public string Name
+        {get; set;}
+        public string ID 
+        {get; set;}
+        public string CGPA
+        {get; set;}
+
+        public student(string n, string id, string cg)
+        {
+            Name = n;
+            ID = id;
+            CGPA = cg;
+        }
+    }
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            student myObj = new student("S. S. Zobaer Ahmed", "22-49415-3", "3.78");
+            Console.WriteLine($"Name = {myObj.Name} \nID = {myObj.ID} \nCGPA = {myObj.CGPA}");
+        }
+    }
+}
