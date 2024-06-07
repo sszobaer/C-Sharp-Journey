34fadb935a93d566a65855813bb93f3b9e3d6192
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-06-07 19:44:17 +0600

    Add files via upload

---
+++ Interface/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace MasterCourse
+{
+    //Interface
+    //Abstraction
+    interface IUniversity
+    {
+        void ID();
+    }
+
+    interface ICourse
+    {
+        void course();
+    }
+    
+    class Student : IUniversity, ICourse
+    {
+        public void ID()
+        {
+            Console.WriteLine("22-49415-3");
+        }
+        public void course()
+        {
+            Console.WriteLine("Object Oriented Programming");
+        }
+    }
+
+    
+    class Program
+    {
+       static void Main(string[] args)
+        {
+            Student stu = new Student();
+            stu.ID();
+            stu.course();
+        }
+    }
+}
