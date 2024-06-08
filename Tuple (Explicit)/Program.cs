5101a7363efe2c8b2fa6c12187344d27fac10663
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-06-08 19:18:41 +0600

    Add files via upload

---
+++ Tuple (Explicit)/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ExplicitTuple
+{
+    class TupleDemo
+    {
+        
+        //Explicit tuple
+        public (int sum, int Product) Math(int x, int y)
+        {
+            int Sum = x+y;//Item1
+            int Pro = x*y;
+            return (Sum, Pro);
+        }
+       
+        static void Main(string[]args)
+        {
+           TupleDemo td = new TupleDemo();
+           var (sumResult, productResult) = td.Math(2, 6);
+            Console.WriteLine(sumResult);
+            Console.WriteLine(productResult);
+        }
+    }
+}
