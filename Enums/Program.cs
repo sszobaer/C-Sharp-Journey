d357a13a0163dcabd742112fe254e93fdfb75886
S. S. Zobaer Ahmed <118447914+sszobaer@users.noreply.github.com>
2024-06-08 19:12:31 +0600

    Add files via upload

---
+++ Enums/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Enums
+{
+    enum level
+    {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday
+    }
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            level Num = level.Thursday;
+            switch (Num)
+            {
+                case level.Saturday:
+                Console.WriteLine("Saturday");
+                break;
+                case level.Sunday:
+                Console.WriteLine("Sunday");
+                break;
+                case level.Monday:
+                Console.WriteLine("Monday");
+                break;
+                case level.Tuesday:
+                Console.WriteLine("Tuesday");
+                break;
+                case level.Wednesday:
+                Console.WriteLine("Wednesday");
+                break;
+                case level.Thursday:
+                Console.WriteLine("Thursday");
+                break;
+                case level.Friday:
+                Console.WriteLine("Friday");
+                break;
+                
+            }
+        }
+    }
+}
